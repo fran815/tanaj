b50bbe41c9d2994b42b529d29d7d0ead109e3565
fran815 <navii.0892@gmail.com>
2025-05-10 08:02:11 -0700

    update 1.1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for
 from bereshit import bereshit_cont
+from shemot import shemot_cont
 
 app = Flask(__name__)
 
@@ -16,6 +17,14 @@ def bereshit():
     phonetics_verses = bereshit_cont
 
     return render_template('bereshit.html', español=spanish_verses, hebreo=hebrew_verses, fonetica=phonetics_verses)
+
+
+@app.route('/shemot')
+def shemot():
+    spanish_verses = shemot_cont
+    hebrew_verses = shemot_cont
+
+    return render_template('shemot.html', español=spanish_verses, hebreo=hebrew_verses)
 
 
 @app.route('/tefilot')
---
+++ shemot.py
@@ -0,0 +1,176 @@
+shemot_cont = {
+    'cap_1': {
+        'spanish': [
+            "Y éstos son los nombres de los hijos de Israel, los vinientes a Egipto con Iahacov, cada hombre y su familia habían venido:",
+            "Reuben, Shimhon, Levi y Iehudah.",
+            "Issajar, Zebulun y Biniamin.",
+            "Dan y Naftali, Gad y Asher.",
+            "Y fue que todas las personas -salidas de los muslos de Iahacov- (eran) setenta personas; empero Iosef ya estaba en Egipto.",
+            "Murió Iosef y todos sus hermanos y toda la generación, aquélla.",
+            "Empero los hijos de Israel fructificaron y proliferaron, se multiplicaron Y se fortalecieron mucho, mucho. Y se llenó la tierra de ellos.",
+            "Se erigió un rey nuevo sobre Egipto, que desconoció a Iosef.",
+            "El dijo a su pueblo: ¡He aquí que el pueblo, los hijos de Israel, es numeroso, y más fuerte que nosotros!",
+            "Vamos, actuemos con astucia respecto a él, no sea que se acreciente. Y sería que cuando sucediere guerra se sumaría, también él, a nuestros adversarios y combatiría contra nosotros y ascendería desde la tierra.",
+            "Nombraron sobre él jefes de tributos : para oprimirlo en sus trabajos forzados y él (pueblo) edificó ciudades depósito para Parhoh: a Pitom y a Rahamses",
+            "Empero, así como lo oprimían, así se acrecentaba y así se expandía. Y se angustiaron a causa de los hijos de Israel.",
+            "E hicieron servir los egipcios a los hijos de Israel con dureza.",
+            "Les amargaron sus vidas con trabajos duros, en argamasa y en ladrillos, y en todo trabajo en el campo. A todos sus trabajos los sometieron, haciéndolos trabajar con dureza.",
+            "Dijo el rey de Egipto a las parteras de las hebreas el nombre de una de las cuales era Shifrah y el nombre de la otra Puhah.",
+            "Dijo él: Cuando parteáreis a las hebreas, habréis de mirar en el asiento de la parturienta; si fuere un hijo ¡ Matadlo! y si fuere una hija ¡ Que viva!",
+            "Empero temieron las parteras a Elohim, y no obraron como les había hablado el rey de Egipto y dejaron vivir a los niños.",
+            "Convocó el rey de Egipto a las parteras y les dijo a ellas: ¿Por qué habéis hecho esta cosa? ¡Habéis dejado vivir a los niños!",
+            "Dijeron las parteras a Parhoh: Porque no son como las mujeres egipcias las hebreas, ya que vigorosas son ellas. Antes que viniere a ellas la partera, ellas ya han dado a luz.",
+            "E hizo bien Elohim, a las parteras .. Se acrecentó el pueblo y se fortalecieron mucho.",
+            "Ocurrió que, como temieron las parteras a Elohim, les hizo a ellos casas.",
+            "Ordenó Parhoh a todo su pueblo diciendo: Todo niño nacido, al río (Nilo) habréis de arrojarlo; empero a toda niña habréis de dejar vivir."
+        ],
+        
+        'hebrew': [
+            'וְאֵ֗לֶּה שְׁמוֹת֙ בְּנֵ֣י יִשְׂרָאֵ֔ל הַבָּאִ֖ים מִצְרָ֑יְמָה אֵ֣ת יַעֲקֹ֔ב אִ֥ישׁ וּבֵית֖וֹ בָּֽאוּ', #1
+            'רְאוּבֵ֣ן שִׁמְע֔וֹן לֵוִ֖י וִיהוּדָֽה', #2
+            'יִשָּׂשכָ֥ר זְבוּלֻ֖ן וּבְנְיָמִֽן', #3
+            'דָּ֥ן וְנַפְתָּלִ֖י גָּ֥ד וְאָשֵֽׁר', #4
+            'וַֽיְהִ֗י כָּל־נֶ֛פֶשׁ יֹצְאֵ֥י יֶֽרֶךְ־יַעֲקֹ֖ב שִׁבְעִ֣ים נָ֑פֶשׁ וְיוֹסֵ֖ף הָיָ֥ה בְמִצְרָֽיִם', #5
+            'וַיָּ֤מָת יוֹסֵף֙ וְכָל־אֶחָ֔יו וְכֹ֖ל הַדּ֥וֹר הַהֽוּא', #6
+            'וּבְנֵ֣י יִשְׂרָאֵ֗ל פָּר֧וּ וַֽיִּשְׁרְצ֛וּ וַיִּרְבּ֥וּ וַיַּֽעַצְמ֖וּ בִּמְאֹ֣ד מְאֹ֑ד וַתִּמָּלֵ֥א הָאָ֖רֶץ אֹתָֽם', #7
+            'וַיָּ֥קָם מֶֽלֶךְ־חָדָ֖שׁ עַל־מִצְרָ֑יִם אֲשֶׁ֥ר לֹֽא־יָדַ֖ע אֶת־יוֹסֵֽף', #8
+            'וַיֹּ֖אמֶר אֶל־עַמּ֑וֹ הִנֵּ֗ה עַ֚ם בְּנֵ֣י יִשְׂרָאֵ֔ל רַ֥ב וְעָצ֖וּם מִמֶּֽנּוּ', #9
+            'הָ֥בָה נִֽתְחַכְּמָ֖ה ל֑וֹ פֶּן־יִרְבֶּ֗ה וְהָיָ֞ה כִּֽי־תִקְרֶ֤אנָה מִלְחָמָה֙ וְנוֹסַ֤ף גַּם־הוּא֙ עַל־שֹׂ֣נְאֵ֔ינוּ וְנִלְחַם־בָּ֖נוּ וְעָלָ֥ה מִן־הָאָֽרֶץ', #10
+            'וַיָּשִׂ֤ימוּ עָלָיו֙ שָׂרֵ֣י מִסִּ֔ים לְמַ֥עַן עַנֹּת֖וֹ בְּסִבְלֹתָ֑ם וַיִּ֜בֶן עָרֵ֤י מִסְכְּנוֹת֙ לְפַרְעֹ֔ה אֶת־פִּתֹ֖ם וְאֶת־רַעַמְסֵֽס', #11
+            'וְכַאֲשֶׁר֙ יְעַנּ֣וּ אֹת֔וֹ כֵּ֥ן יִרְבֶּ֖ה וְכֵ֣ן יִפְרֹ֑ץ וַיָּקֻ֕צוּ מִפְּנֵ֖י בְּנֵ֥י יִשְׂרָאֵֽל', #12
+            'וַיַּעֲבִ֧דוּ מִצְרַ֛יִם אֶת־בְּנֵ֥י יִשְׂרָאֵ֖ל בְּפָֽרֶךְ', #13
+            'וַיְמָרְר֨וּ אֶת־חַיֵּיהֶ֜ם בַּעֲבֹדָ֣ה קָשָׁ֗ה בְּחֹ֙מֶר֙ וּבִלְבֵנִ֔ים וּבְכָל־עֲבֹדָ֖ה בַּשָּׂדֶ֑ה אֵ֚ת כָּל־עֲבֹ֣דָתָ֔ם אֲשֶׁר־עָבְד֥וּ בָהֶ֖ם בְּפָֽרֶךְ', #14
+            'וַיֹּ֙אמֶר֙ מֶ֣לֶךְ מִצְרַ֔יִם לַֽמְיַלְּדֹ֖ת הָֽעִבְרִיֹּ֑ת אֲשֶׁ֨ר שֵׁ֤ם הָֽאַחַת֙ שִׁפְרָ֔ה וְשֵׁ֥ם הַשֵּׁנִ֖ית פּוּעָֽה', #15
+            'וַיֹּ֗אמֶר בְּיַלֶּדְכֶן֙ אֶת־הָֽעִבְרִיּ֔וֹת וּרְאִיתֶ֖ן עַל־הָאָבְנָ֑יִם אִם־בֵּ֥ן הוּא֙ וַהֲמִתֶּ֣ן אֹת֔וֹ וְאִם־בַּ֥ת הִ֖יא וָחָֽיָה', #16
+            'וַתִּירֶ֤אןָ הַֽמְיַלְּדֹת֙ אֶת־הָ֣אֱלֹהִ֔ים וְלֹ֣א עָשׂ֔וּ כַּאֲשֶׁ֛ר דִּבֶּ֥ר אֲלֵיהֶ֖ן מֶ֣לֶךְ מִצְרָ֑יִם וַתְּחַיֶּ֖יןָ אֶת־הַיְלָדִֽים', #17
+            'וַיִּקְרָ֤א מֶֽלֶךְ־מִצְרַ֙יִם֙ לַֽמְיַלְּדֹ֔ת וַיֹּ֣אמֶר לָהֶ֔ן מַדּ֥וּעַ עֲשִׂיתֶ֖ן הַדָּבָ֣ר הַזֶּ֑ה וַתְּחַיֶּ֖יןָ אֶת־הַיְלָדִֽים', #18
+            'וַתֹּאמַ֤רְןָ הַֽמְיַלְּדֹת֙ אֶל־פַּרְעֹ֔ה כִּ֣י לֹ֧א כַנָּשִׁ֛ים הַמִּצְרִיֹּ֖ת הָֽעִבְרִיֹּ֑ת כִּֽי־חָי֣וֹת הֵ֔נָּה בְּטֶ֨רֶם תָּב֧וֹא אֲלֵהֶ֛ן הַמְיַלֶּ֖דֶת וְיָלָֽדוּ', #19
+            'וַיֵּ֥יטֶב אֱלֹהִ֖ים לַֽמְיַלְּדֹ֑ת וַיִּ֧רֶב הָעָ֛ם וַיַּֽעַצְמ֖וּ מְאֹֽד', #20
+            'וַיְהִ֕י כִּֽי־יָֽרְא֥וּ הַֽמְיַלְּדֹ֖ת אֶת־הָאֱלֹהִ֑ים וַיַּ֥עַשׂ לָהֶ֖ם בָּתִּֽים', #21
+            'וַיְצַ֣ו פַּרְעֹ֔ה לְכָל־עַמּ֖וֹ לֵאמֹ֑ר כָּל־הַבֵּ֣ן הַיִּלּ֗וֹד הַיְאֹ֙רָה֙ תַּשְׁלִיכֻ֔הוּ וְכָל־הַבַּ֖ת תְּחַיּֽוּן', #22
+        ]
+    },
+
+    'cap_2': {
+        'spanish': [
+            'Fue un hombre de la familia de Levi y desposó a la hija de Levi', #1
+            'Quedó encinta la mujer Y dio a luz un hijo. Vio que era hermoso Y ella lo escondió durante tres meses', #2
+            'Empero no pudo ella esconderlo más y tomó ella para él una cesta de papiro y la recubrió con betún y con pez, puso en ella al niño y la depositó entre los juncos, en la ribera del río(Nilo)', #3
+            'Y se apostó su hermana, a lo lejos, para saber qué le sería hecho, a él', #4
+            'Mas descendió la hija de Parhoh para lavarse en el río ,mientras sus mozas andaban por la ribera del río. Ella divisó la cesta entre los juncos, mandó a su sierva y la recogió', #5
+            'Ella abrió y lo vio -al infante- y : aquí que un niño lloraba. Ella tuvo compasión por él y dijo: ¡De los niños hebreos es éste!', #6
+            'Le dijo su hermana a la hija de Parhoh: ¿Habré de ir a llamar para ti una mujer nodriza, de las hebreas para que te amamante al niño?', #7
+            'Le dijo la hija de Parhoh: Ve. Fue la moza y llamó a la madre del niño', #8
+            'Le dijo a ella la hija de Parhoh: Lleva a este niño y amamántamelo y yo daré tu paga. Tomó la mujer al niño y lo amamantó', #9
+            'Creció el niño y ella se lo trajo a la hija de Parhoh; y fue para ella, hijo. Ella llamó su nombre "Mosheh" pues ella dijo: De las aguas yo lo he extraído', #10
+            'y ocurrió en aquellos días, que había crecido Mosheh y salió hacia sus hermanos y observó sus trabajos forzados; y vio a un hombre egipcio que pegaba a un hombre hebreo, de sus hermanos', #11
+            'Se tornó hacia acá y hacia allá y él vio que no había hombre alguno, batió al egipcio y lo ocultó en la arena', #12
+            'Salió al segundo día y he aquí que los hombres hebreos contendían y dijo al impío: ¿Por qué has de pegar a tu prójimo?', #13
+            'Dijo él: ¿ Quién te ha designado por hombre jefe y juez sobre nosotros? ¿Acaso matarme tú piensas, como mataste al egipcio? Temió Mosheh y pensó: Ciertamente se ha sabido la cosa', #14
+            'Oyó Parhoh esta cosa Y quiso matar a Mosheh; empero huyó Mosheh de ante Parhoh y se asentó en la tierra de Midian. Se sentó allí a la vera del pozo', #15
+            'y el sacerdote de Midian tenía siete hijas; ellas vinieron, sacaron agua del pozo y colmaron las pilas para abrevar a las ovejas de su padre', #16
+            'Empero vinieron los pastores y las expulsaron; mas se levantó Mosheh y las socorrió y abrevó a sus ovejas', #17
+            'Ellas vinieron a Rehuel, su padre. El dijo: ¿Por qué os habéis apresurado en venir hoy?', #18
+            'Dijeron ellas: Un hombre egipcio nos ha librado de mano de los pastores y también sacar ha sacado agua del pozo para nosotras y abrevó a las ovejas', #19
+            'Dijo él a sus hijas: ¿Y dónde está él? ¿Por qué es que habéis dejado al hombre? Llamadlo y que coma comida', #20
+            'Asintió Mosheh permanecer con el hombre y él dio a Tsiporah, su hija, a Mosheh (como esposa)', #21
+            'Ella dio a luz un hijo y él llamó su nombre Guereshom, pues él dijo: Peregrino he sido en tierra extranjera', #22
+            'Y ocurrió que en el transcurso de los numerosos días, aquéllos, murió el rey de Egipto y gimieron los hijos de Israel por causa de la servidumbre; y clamaron y ascendió su imploración hacia Elohim por causa de la servidumbre', #23
+            'Oyó Elohim su quejido y recordó Elohim Su Pacto con Abraham, con Itsjak y con lahacov', #24
+            'Vio Elohim a los hijos de Israel y se apiadó Elohim' #25
+        ],
+        'hebrew': [
+            'וַיֵּ֥לֶךְ אִ֖ישׁ מִבֵּ֣ית לֵוִ֑י וַיִּקַּ֖ח אֶת־בַּת־לֵוִֽי', #1
+            'וַתַּ֥הַר הָאִשָּׁ֖ה וַתֵּ֣לֶד בֵּ֑ן וַתֵּ֤רֶא אֹתוֹ֙ כִּי־ט֣וֹב ה֔וּא וַֽתִּצְפְּנֵ֖הוּ שְׁלֹשָׁ֥ה יְרָחִֽים', #2
+            'וְלֹא־יָכְלָ֣ה עוֹד֮ הַצְּפִינוֹ֒ וַתִּֽקַּֽח־לוֹ֙ תֵּ֣בַת גֹּ֔מֶא וַתַּחְמְרָ֥ה בַחֵמָ֖ר וּבַזָּ֑פֶת וַתָּ֤שֶׂם בָּהּ֙ אֶת־הַיֶּ֔לֶד וַתָּ֥שֶׂם בַּסּ֖וּף עַל־שְׂפַ֥ת הַיְאֹֽר', #3
+            'וַתֵּתַצַּ֥ב אֲחֹת֖וֹ מֵרָחֹ֑ק לְדֵעָ֕ה מַה־יֵּעָשֶׂ֖ה לֽוֹ', #4
+            'וַתֵּ֤רֶד בַּת־פַּרְעֹה֙ לִרְחֹ֣ץ עַל־הַיְאֹ֔ר וְנַעֲרֹתֶ֥יהָ הֹלְכֹ֖ת עַל־יַ֣ד הַיְאֹ֑ר וַתֵּ֤רֶא אֶת־הַתֵּבָה֙ בְּת֣וֹךְ הַסּ֔וּף וַתִּשְׁלַ֥ח אֶת־אֲמָתָ֖הּ וַתִּקָּחֶֽהָ', #5
+            'וַתִּפְתַּח֙ וַתִּרְאֵ֣הוּ אֶת־הַיֶּ֔לֶד וְהִנֵּה־נַ֖עַר בֹּכֶ֑ה וַתַּחְמֹ֣ל עָלָ֔יו וַתֹּ֕אמֶר מִיַּלְדֵ֥י הָֽעִבְרִ֖ים זֶֽה', #6
+            'וַתֹּ֣אמֶר אֲחֹתוֹ֮ אֶל־בַּת־פַּרְעֹה֒ הַאֵלֵ֗ךְ וְקָרָ֤אתִי לָךְ֙ אִשָּׁ֣ה מֵינֶ֔קֶת מִ֖ן הָעִבְרִיֹּ֑ת וְתֵינִ֥ק לָ֖ךְ אֶת־הַיָּֽלֶד', #7
+            'וַתֹּֽאמֶר־לָ֥הּ בַּת־פַּרְעֹ֖ה לֵ֑כִי וַתֵּ֙לֶךְ֙ הָֽעַלְמָ֔ה וַתִּקְרָ֖א אֶת־אֵ֥ם הַיָּֽלֶד', #8
+            'וַתֹּ֧אמֶר לָ֣הּ בַּת־פַּרְעֹ֗ה הֵילִ֜יכִי אֶת־הַיֶּ֤לֶד הַזֶּה֙ וְהֵינִקִ֣הוּ לִ֔י וַאֲנִ֖י אֶתֵּ֣ן אֶת־שְׂכָרֵ֑ךְ וַתִּקַּ֧ח הָאִשָּׁ֛ה הַיֶּ֖לֶד וַתְּנִיקֵֽהוּ', #9
+            'וַיִגְדַּ֣ל הַיֶּ֗לֶד וַתְּבִאֵ֙הוּ֙ לְבַת־פַּרְעֹ֔ה וַֽיְהִי־לָ֖הּ לְבֵ֑ן וַתִּקְרָ֤א שְׁמוֹ֙ מֹשֶׁ֔ה וַתֹּ֕אמֶר כִּ֥י מִן־הַמַּ֖יִם מְשִׁיתִֽהוּ', #10
+            'וַיְהִ֣י בַּיָּמִ֣ים הָהֵ֗ם וַיִּגְדַּ֤ל מֹשֶׁה֙ וַיֵּצֵ֣א אֶל־אֶחָ֔יו וַיַּ֖רְא בְּסִבְלֹתָ֑ם וַיַּרְא֙ אִ֣ישׁ מִצְרִ֔י מַכֶּ֥ה אִישׁ־עִבְרִ֖י מֵאֶחָֽיו', #11
+            'וַיִּ֤פֶן כֹּה֙ וָכֹ֔ה וַיַּ֖רְא כִּ֣י אֵ֣ין אִ֑ישׁ וַיַּךְ֙ אֶת־הַמִּצְרִ֔י וַֽיִּטְמְנֵ֖הוּ בַּחֽוֹל', #12
+            'וַיֵּצֵא֙ בַּיּ֣וֹם הַשֵּׁנִ֔י וְהִנֵּ֛ה שְׁנֵֽי־אֲנָשִׁ֥ים עִבְרִ֖ים נִצִּ֑ים וַיֹּ֙אמֶר֙ לָֽרָשָׁ֔ע לָ֥מָּה תַכֶּ֖ה רֵעֶֽךָ', #13
+            'וַ֠יֹּאמֶר מִ֣י שָֽׂמְךָ֞ לְאִ֨ישׁ שַׂ֤ר וְשֹׁפֵט֙ עָלֵ֔ינוּ הַלְהָרְגֵ֙נִי֙ אַתָּ֣ה אֹמֵ֔ר כַּאֲשֶׁ֥ר הָרַ֖גְתָּ אֶת־הַמִּצְרִ֑י וַיִּירָ֤א מֹשֶׁה֙ וַיֹּאמַ֔ר אָכֵ֖ן נוֹדַ֥ע הַדָּבָֽר', #14
+            'וַיִּשְׁמַ֤ע פַּרְעֹה֙ אֶת־הַדָּבָ֣ר הַזֶּ֔ה וַיְבַקֵּ֖שׁ לַהֲרֹ֣ג אֶת־מֹשֶׁ֑ה וַיִּבְרַ֤ח מֹשֶׁה֙ מִפְּנֵ֣י פַרְעֹ֔ה וַיֵּ֥שֶׁב בְּאֶֽרֶץ־מִדְיָ֖ן וַיֵּ֥שֶׁב עַֽל־הַבְּאֵֽר', #15
+            'וּלְכֹהֵ֥ן מִדְיָ֖ן שֶׁ֣בַע בָּנ֑וֹת וַתָּבֹ֣אנָה וַתִּדְלֶ֗נָה וַתְּמַלֶּ֙אנָה֙ אֶת־הָ֣רְהָטִ֔ים לְהַשְׁק֖וֹת צֹ֥אן אֲבִיהֶֽן', #16
+            'וַיָּבֹ֥אוּ הָרֹעִ֖ים וַיְגָרְשׁ֑וּם וַיָּ֤קָם מֹשֶׁה֙ וַיּ֣וֹשִׁעָ֔ן וַיַּ֖שְׁקְ אֶת־צֹאנָֽם', #17
+            'וַתָּבֹ֕אנָה אֶל־רְעוּאֵ֖ל אֲבִיהֶ֑ן וַיֹּ֕אמֶר מַדּ֛וּעַ מִהַרְתֶּ֥ן בֹּ֖א הַיּֽוֹם', #18
+            'וַתֹּאמַ֕רְןָ אִ֣ישׁ מִצְרִ֔י הִצִּילָ֖נוּ מִיַּ֣ד הָרֹעִ֑ים וְגַם־דָּלֹ֤ה דָלָה֙ לָ֔נוּ וַיַּ֖שְׁקְ אֶת־הַצֹּֽאן', #19
+            'וַיֹּ֥אמֶר אֶל־בְּנֹתָ֖יו וְאַיּ֑וֹ לָ֤מָּה זֶּה֙ עֲזַבְתֶּ֣ן אֶת־הָאִ֔ישׁ קִרְאֶ֥ן ל֖וֹ וְיֹ֥אכַל לָֽחֶם', #20
+            'וַיּ֥וֹאֶל מֹשֶׁ֖ה לָשֶׁ֣בֶת אֶת־הָאִ֑ישׁ וַיִּתֵּ֛ן אֶת־צִפֹּרָ֥ה בִתּ֖וֹ לְמֹשֶֽׁה', #21
+            'וַתֵּ֣לֶד בֵּ֔ן וַיִּקְרָ֥א אֶת־שְׁמ֖וֹ גֵּרְשֹׁ֑ם כִּ֣י אָמַ֔ר גֵּ֣ר הָיִ֔יתִי בְּאֶ֖רֶץ נָכְרִיָּֽה', #22
+            'וַיְהִי֩ בַיָּמִ֨ים הָֽרַבִּ֜ים הָהֵ֗ם וַיָּ֙מָת֙ מֶ֣לֶךְ מִצְרַ֔יִם וַיֵּאָנְח֧וּ בְנֵֽי־יִשְׂרָאֵ֛ל מִן־הָעֲבֹדָ֖ה וַיִּזְעָ֑קוּ וַתַּ֧עַל שַׁוְעָתָ֛ם אֶל־הָאֱלֹהִ֖ים מִן־הָעֲבֹדָֽה', #23
+            'וַיִּשְׁמַ֥ע אֱלֹהִ֖ים אֶת־נַאֲקָתָ֑ם וַיִּזְכֹּ֤ר אֱלֹהִים֙ אֶת־בְּרִית֔וֹ אֶת־אַבְרָהָ֖ם אֶת־יִצְחָ֥ק וְאֶֽת־יַעֲקֹֽב', #24
+            'וַיַּ֥רְא אֱלֹהִ֖ים אֶת־בְּנֵ֣י יִשְׂרָאֵ֑ל וַיֵּ֖דַע אֱלֹהִֽים' #25
+        ]
+    }
+
+}
+
+
+"""
+'cap_x': {
+        'spanish': [],
+        'hebrew': []
+    }
+
+
+'', #1
+'', #2
+'', #3
+'', #4
+'', #5
+'', #6
+'', #7
+'', #8
+'', #9
+'', #10
+'', #11
+'', #12
+'', #13
+'', #14
+'', #15
+'', #16
+'', #17
+'', #18
+'', #19
+'', #20
+'', #21
+'', #22
+'', #23
+'', #24
+'', #25
+'', #26
+'', #27
+'', #28
+'', #29
+'', #30
+'', #31
+'', #32
+'', #33
+'', #34
+'', #35
+'', #36
+'', #37
+'', #39
+'', #40
+'', #41
+'', #42
+'', #43
+'', #44
+'', #45
+'', #46
+'', #47
+'', #48
+'', #49
+'', #50
+'', #51
+'', #52
+'', #53
+'', #54
+'', #55
+
+"""
